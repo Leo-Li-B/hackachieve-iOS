c7e86681e669852fa14347cad0fc2c7bc08e136c
Leo Li <leoli@leos-mbp.hitronhub.home>
2019-02-08 00:44:58 -0800

    Initial Commit

---
+++ UserLoginAndRegistration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by Leo Li on 2019-02-08.
+//  Copyright © 2019 Leo Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
